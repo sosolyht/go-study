3af5787515b0a0050b5b2013b6f7dc700f608e80
sosolyht <sosolyht@gmail.com>
2023-03-02 22:39:06 +0900

    post , put, delete 퀴즈 추가

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"echo/database"
 	"echo/database/entity"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"log"
 	"net/http"
 )
@@ -13,7 +12,6 @@ func main() {
 	database.DBConnection()
 
 	e := echo.New()
-	e.Use(middleware.Logger())
 
 	// hello world 예시
 	e.GET("/hello", func(c echo.Context) error {
@@ -24,13 +22,20 @@ func main() {
 	e.GET("/users", fetchUsers)
 	e.GET("/users/:id", getUser)
 
+	// 직접 해보아요
+	e.POST("/users/:id", createUser)
+	e.PUT("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
+
 	log.Fatal(e.Start(":3000"))
 }
 
 func fetchUsers(c echo.Context) error {
 	db := database.DBConnection()
-
 	var users []entity.Users
+
+	// 모든 유저의 레코드
+	// 참고: https://gorm.io/docs/query.html
 	db.Find(&users)
 
 	return c.JSON(http.StatusOK, users)
@@ -39,17 +44,28 @@ func fetchUsers(c echo.Context) error {
 func getUser(c echo.Context) error {
 	db := database.DBConnection()
 	var users entity.Users
-	var binder struct {
-		Id int64 `json:"-" xml:"-" param:"id"`
-	}
-	
-	if err := c.Bind(&binder); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{
-			"message": "bad request",
-		})
-	}
 
-	db.Where("id = ?", binder.Id).Find(&users)
+	// path parameters
+	// 참고: https://echo.labstack.com/guide/request/#path-parameters
+	id := c.Param("id")
+
+	db.Where("id = ?", id).Find(&users)
 
 	return c.JSON(http.StatusOK, users)
+}
+
+// 아래를 채워주세요
+// 공식문서 참고
+// echo: https://echo.labstack.com/guide/
+// gorm: https://gorm.io/ko_KR/docs/index.html
+func createUser(c echo.Context) error {
+	return nil
+}
+
+func updateUser(c echo.Context) error {
+	return nil
+}
+
+func deleteUser(c echo.Context) error {
+	return nil
 }
