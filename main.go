26d8ac75a5f1f4ab9db5417a470e43c7ba741fc9
sosolyht <sosolyht@gmail.com>
2023-03-02 19:16:17 +0900

    hello world

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"echo/database"
+	"github.com/labstack/echo/v4"
+	"log"
+	"net/http"
 )
 
 func main() {
 	database.DBConnection()
+
+	e := echo.New()
+
+	e.GET("/hello", func(c echo.Context) error {
+		return c.String(http.StatusOK, "hello world")
+	})
+
+	log.Fatal(e.Start(":3000"))
 }
